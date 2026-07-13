9342cbe95957a1539f54fe2ba2664f447c49bf3e
imaan <imaanally123@gmail.com>
2026-07-13 12:08:46 +0300

    pass all jest tests

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 
+require("datejs");
 
+function combineUsers(...args) {
+      const combinedObject = {
+        users: [],
+      };
+
+      for (const array of args) {
+            combinedObject.users = [
+              ...combinedObject.users, 
+              ...array
+            ];
+      }
+
+      combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+      return combinedObject;
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
